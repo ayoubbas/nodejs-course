4e9e85e8c5373e417f6ae942003f34aea06fd78e
ayoubbas <ayoub-ok147@hotmail.fr>
2025-01-12 23:04:13 +0100

    up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
@@ -9,3 +10,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://hichamok147:OsGeMLMjjdspNoDO@cluster0.e0hqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((res) => {})
+  .catch((err) => {
+    console.log(err);
+  });
