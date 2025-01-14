18c4cf600afa284cb4c8a02e39aeb88fdec970cd
ayoubbas <ayoub-ok147@hotmail.fr>
2025-01-14 14:38:40 +0100

    up

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const path = require("path");
+app.use(express.static(path.join(__dirname, "views")));
 app.use(express.urlencoded({ extended: true }));
 
 const Mydata = require("./models/mydataSchema");
@@ -27,6 +29,55 @@ app.post("/", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
   console.log(req.body);
   const mydata = new Mydata(req.body);
-  mydata.save();
-  res.redirect("/index.html");
+  mydata
+    .save()
+    .then(() => {
+      res.redirect("/success.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// const mongoose = require("mongoose");
+// const path = require("path"); // Import the path module
+
+// app.use(express.urlencoded({ extended: true }));
+
+// // Serve static files from the "views" directory
+// app.use(express.static(path.join(__dirname, "views")));
+
+// const Mydata = require("./models/mydataSchema");
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
+
+// app.listen(port, () => {
+//   console.log(`http://localhost:${port}/`);
+// });
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://hichamok147:OsGeMLMjjdspNoDO@cluster0.e0hqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//   )
+//   .then((res) => {})
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+//   const mydata = new Mydata(req.body);
+//   mydata
+//     .save()
+//     .then(() => {
+//       // Redirect to the success.html file
+//       res.redirect("/success.html");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
