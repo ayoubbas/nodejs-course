411a17a15d2465081dfdc5d968c4f7678ab942e0
ayoubbas <ayoub-ok147@hotmail.fr>
2025-01-14 14:42:11 +0100

    upda

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+app.set("view engine", "ejs");
+
 const path = require("path");
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.urlencoded({ extended: true }));
